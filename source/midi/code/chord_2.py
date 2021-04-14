32d51d199a889771494f1709135d93f23f303b0a
LEON <61219917+1119204640@users.noreply.github.com>
2021-04-14 15:48:44 +0800

    Add files via upload

---
+++ source/midi/code/chord_2.py
@@ -0,0 +1,98 @@
+import mido
+from mido import Message, MidiFile, MidiTrack, MetaMessage
+
+bpm = 105
+tempo = mido.bpm2tempo(bpm)
+meta_time = MetaMessage('time_signature', numerator=6, denominator=8)
+meta_tempo = MetaMessage('set_tempo', tempo = tempo, time=0)
+meta_tone = MetaMessage('key_signature', key='C')
+
+mid = MidiFile()
+track = MidiTrack()
+mid.tracks.append(track)
+
+def play_note(note, length, track, base_num=0, delay=0, velocity=1.0, channel=0):
+   meta_time = 60 * 60 * 10 / bpm
+   major_notes = [0, 2, 2, 1, 2, 2, 2, 1]
+   base_note = 60
+   track.append(Message('note_on', note=base_note + base_num*12 + sum(major_notes[0:note]), velocity=round(64*velocity), time=round(delay*meta_time), channel=channel))
+   track.append(Message('note_off', note=base_note + base_num*12 + sum(major_notes[0:note]), velocity=round(64*velocity), time=round(meta_time*length), channel=channel))
+    
+#前奏
+def prelude():
+    play_note(1, 3, track, velocity=0)
+    play_note(3, 3, track, -1)
+    
+    play_note(1, 3, track, velocity=0)
+    play_note(3, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(1, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(2, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(3, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(1, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(2, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(2, 3, track, -1)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(3, 3, track, -1)
+
+    play_note(1, 6, track, velocity=0)
+
+#主歌
+def precorus():
+    play_note(2, 3, track, -1)
+    play_note(1, 3, track, velocity=0)
+
+    play_note(1, 42, track, velocity=0)
+
+    play_note(2, 3, track, -1)
+    play_note(1, 3, track, velocity=0)
+
+    play_note(1, 42, track, velocity=0)
+
+#副歌
+def corus():
+    play_note(1, 6, track, velocity=0)
+
+    play_note(1, 4, track, velocity=0)
+    play_note(2, 5, track, -1)
+
+    play_note(1, 6, track, velocity=0)
+
+    play_note(1, 4, track, velocity=0)
+    play_note(1, 5, track, -1)
+
+    play_note(1, 24, track, velocity=0)
+
+#尾奏
+def postlude():
+    play_note(1, 12, track, velocity=0)
+
+    play_note(1, 3, track, velocity=0)
+    play_note(3, 3, track)
+
+    play_note(5, 6, track, -1)
+
+    play_note(1, 6, track, velocity=0)
+
+def verse(track):
+    prelude()
+    precorus()
+    corus()
+    corus()
+    postlude()
+
+verse(track)  
+
+mid.save('chord_2.mid')
---
+++ source/midi/code/melody.py
@@ -0,0 +1,209 @@
+import mido
+from mido import Message, MidiFile, MidiTrack, MetaMessage
+
+bpm = 105
+tempo = mido.bpm2tempo(bpm)
+meta_time = MetaMessage('time_signature', numerator=6, denominator=8)
+meta_tempo = MetaMessage('set_tempo', tempo = tempo, time=0)
+meta_tone = MetaMessage('key_signature', key='C')
+
+mid = MidiFile()
+track = MidiTrack()
+mid.tracks.append(track)
+
+def play_note(note, length, track, base_num=0, delay=0, velocity=1.0, channel=0):
+   meta_time = 60 * 60 * 10 / bpm
+   major_notes = [0, 2, 2, 1, 2, 2, 2, 1]
+   base_note = 60
+   track.append(Message('note_on', note=base_note + base_num*12 + sum(major_notes[0:note]), velocity=round(64*velocity), time=round(delay*meta_time), channel=channel))
+   track.append(Message('note_off', note=base_note + base_num*12 + sum(major_notes[0:note]), velocity=round(64*velocity), time=round(meta_time*length), channel=channel))
+    
+#前奏
+def prelude():
+    play_note(1, 2, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(1, 1, track, 1)
+
+    play_note(7, 1, track)  
+    play_note(6, 1, track)  
+    play_note(3, 1, track)
+    play_note(5, 3, track)
+    
+    play_note(6, 1, track)  
+    play_note(7, 1, track)
+    play_note(1, 1, track, 1)
+    play_note(4, 1, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(1, 1, track, 1)
+    
+    play_note(2, 6, track, 1)
+
+    play_note(1, 2, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(1, 1, track, 1)
+
+    play_note(7, 1, track)  
+    play_note(5, 1, track)  
+    play_note(3, 1, track)
+    play_note(6, 3, track)
+
+    play_note(5, 1.5, track)
+    play_note(6, 0.5, track)
+    play_note(7, 0.5, track)
+    play_note(1, 0.5, track, 1)
+    play_note(2, 2, track, 1)
+    play_note(3, 1, track, 1)
+
+    play_note(4, 4, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(2, 1, track, 1)
+
+    play_note(1, 12, track, 1)
+
+#主歌
+def precorus():
+    play_note(5, 1, track)
+    play_note(6, 1, track)
+    play_note(5, 1, track)
+    play_note(4, 1, track)
+    play_note(3, 1, track)
+    play_note(2, 1, track)
+
+    play_note(1, 3, track)
+    play_note(5, 2, track, -1)
+    play_note(5, 1, track, velocity=0)
+
+    play_note(1, 1, track)
+    play_note(3, 1, track)
+    play_note(1, 1, track, 1)
+    play_note(7, 1, track)
+    play_note(6, 1.5, track)
+    play_note(3, 0.5, track)
+
+    play_note(5, 6, track)
+
+    play_note(6, 1, track)
+    play_note(7, 1, track)
+    play_note(6, 1, track)
+    play_note(5, 1, track)
+    play_note(4, 1, track)
+    play_note(3, 1, track)
+
+    play_note(2, 3, track)
+    play_note(6, 3, track, -1)
+
+    play_note(7, 1, track, -1)
+    play_note(6, 1, track, -1)
+    play_note(5, 1, track, -1)
+    play_note(5, 1, track)
+    play_note(1, 1.5, track)
+    play_note(2, 0.5, track)
+
+    play_note(3, 6, track)
+
+    play_note(5, 1, track)
+    play_note(6, 1, track)
+    play_note(5, 1, track)
+    play_note(4, 1, track)
+    play_note(3, 1, track)
+    play_note(2, 1, track)
+
+    play_note(1, 3, track)
+    play_note(5, 2, track, -1)
+    play_note(5, 1, track, velocity=0)
+
+    play_note(1, 1, track)
+    play_note(3, 1, track)
+    play_note(1, 1, track, 1)
+    play_note(7, 1, track)
+    play_note(2, 1.5, track, 1)
+    play_note(1, 0.5, track, 1)
+
+    play_note(6, 6, track)
+
+    play_note(1, 1, track, 1)
+    play_note(7, 1, track)
+    play_note(6, 1, track)
+    play_note(5, 3, track)
+
+    play_note(6, 1, track)
+    play_note(5, 1, track)
+    play_note(4, 1, track)
+    play_note(3, 3, track)
+
+    play_note(7, 2, track, -1)
+    play_note(6, 1, track, -1)
+    play_note(5, 2, track, -1)
+    play_note(2, 0.5, track)
+    play_note(1, 0.5, track)
+
+    play_note(1, 6, track)
+
+#副歌
+def corus():
+    play_note(1, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(1, 1, track, 1)
+    play_note(6, 1, track)
+
+    #转为9/8拍
+    play_note(7, 1, track)
+    play_note(6, 1.5, track)
+    play_note(3, 0.5, track)
+    play_note(5, 6, track)
+
+    #回到6/8拍
+    play_note(1, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(3, 1, track, 1)
+    play_note(2, 1, track, 1)
+    play_note(1, 1, track, 1)
+    play_note(6, 1, track)
+
+    #转为9/8拍
+    play_note(7, 1, track)
+    play_note(5, 1.5, track)
+    play_note(3, 0.5, track)
+    play_note(6, 6, track)
+
+    #回到6/8拍
+    play_note(5, 1, track)
+    play_note(4, 1, track)
+    play_note(3, 1, track)
+    play_note(2, 3, track)
+
+    play_note(7, 1, track, -1)
+    play_note(6, 1, track, -1)
+    play_note(5, 1, track, -1)
+    play_note(3, 3, track)
+
+    play_note(4, 3, track)
+    play_note(2, 2, track)
+    play_note(1, 1, track)
+
+    play_note(1, 6, track)
+
+#尾奏
+def postlude():
+    play_note(4, 3, track)
+    play_note(2, 8, track, 1)
+    play_note(1, 1, track, 1)
+    play_note(1, 15, track, 1)
+    play_note(1, 3, track, 1, velocity=0)
+
+def verse(track):
+    prelude()
+    precorus()
+    corus()
+    corus()
+    postlude()
+
+verse(track)  
+
+mid.save('melody.mid')
